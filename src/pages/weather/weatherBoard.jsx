2b83732cb7e0e8db3919409fddf98f6d8309e2ff
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-05-28 20:25:43 +0600

    error fixed

--- src/pages/weather/weatherBoard.jsx
+++ src/pages/weather/weatherBoard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AddToFavourite from "./addToFavourite";
 import WeatherHeadline from "./weatherHeadline";
+import WeatherCondition from "./weatherCondition";
 
 export default function WeatherBoard() {
   return (
@@ -9,8 +10,8 @@ export default function WeatherBoard() {
         <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
           <div className="grid md:grid-cols-2 gap-10 md:gap-6">
             <AddToFavourite />
-
             <WeatherHeadline />
+            <WeatherCondition />
           </div>
         </div>
       </div>
